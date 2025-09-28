658b9423376c5c537290afc8a5a903ecb73e8ff1
Suryateja Challa <surtecha@gmail.com>
2025-09-28 15:32:24 +0530

    update: use theme colors and adapt to default accent

---
+++ lib/components/common/floating_action_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:karman/theme/color_scheme.dart';
+import 'package:karman/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
+
+class CustomFloatingActionButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const CustomFloatingActionButton({
+    super.key,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeProvider>(
+      builder: (context, theme, child) {
+        return GestureDetector(
+          onTap: onPressed,
+          child: Container(
+            width: 56,
+            height: 56,
+            decoration: BoxDecoration(
+              color: AppColorScheme.accent(theme, context),
+              shape: BoxShape.circle,
+            ),
+            child: Icon(
+              CupertinoIcons.add,
+              color: theme.isDark ? CupertinoColors.black : CupertinoColors.white,
+              size: 24,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
